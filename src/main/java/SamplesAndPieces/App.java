2e2a32a87c17c95a7e0139fa4c3b61e50de90f61
Alexandr Panov <a.panov@program-ace.com.ua>
2017-11-01 18:29:18 +0200

    ~App

--- src/main/java/SamplesAndPieces/App.java
+++ src/main/java/SamplesAndPieces/App.java
@@ -1,13 +1,33 @@
 package SamplesAndPieces;
 
+import scala.Int;
+
 /**
  * Hello world!
- *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+
+
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
+
+    /**
+     * Допустимые символы в именах переменных
+     */
+    static void variableNames()
+    {
+        int $_123строка3_A = 44;
+
+        System.out.println($_123строка3_A);
+    }
+
+
+
+
+
+
+
 }
