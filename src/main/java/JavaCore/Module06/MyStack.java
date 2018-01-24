ac9f0bc6253140e381c85bd12ade8ed4bdb1f82c
wonderer@i.ua <alex panov>
2018-01-24 21:31:27 +0200

    +My[collections]

---
+++ src/main/java/JavaCore/Module06/MyStack.java
@@ -0,0 +1,80 @@
+package JavaCore.Module06;
+
+/**
+ * https://javadevblog.com/stek-s-ispol-zovaniem-svyazannogo-spiska-na-java.html
+ *
+ * Задание 4 - Stack
+ * Написать свой класс MyStack как аналог классу Stack.
+ * <p>
+ * LIFO (last-in-first-out)
+ * <p>
+ * Можно делать либо с помощью Node либо с помощью массива.
+ * <p>
+ * Методы
+ * push(T value) добавляет элемент в конец
+ * <p>
+ * remove(int index) удаляет элемент под индексом
+ * <p>
+ * clear() очищает коллекцию
+ * <p>
+ * size() возвращает размер коллекции
+ * <p>
+ * peek() возвращает первый элемент в стеке (LIFO)
+ * <p>
+ * pop() возвращает первый элемент в стеке и удаляет его из коллекции
+ */
+public class MyStack<T>
+{
+
+    /**
+     * todo  добавляет элемент в конец
+     */
+    public void push(T value)
+    {
+
+    }
+
+    /**
+     * todo удаляет элемент под индексом
+     */
+    public void remove(int index)
+    {
+
+    }
+
+    /**
+     * todo очищает коллекцию
+     */
+    public void clear()
+    {
+
+    }
+
+    /**
+     * todo возвращает размер коллекции
+     */
+    public int size()
+    {
+
+        return 0;
+    }
+
+    /**
+     * todo возвращает первый элемент в стеке (LIFO)
+     */
+    public T peek()
+    {
+
+        return null;
+    }
+
+    /**
+     * todo возвращает первый элемент в стеке и удаляет его из коллекции
+     */
+    public T pop()
+    {
+        return null;
+    }
+
+}
+
