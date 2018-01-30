6a2b7941c536f5f866417ed9f0f1d6b09f55a8cb
wonderer@i.ua <Alex Panov>
2018-01-30 21:45:02 +0200

    ~App

--- src/main/java/JavaCore/Module06/App.java
+++ src/main/java/JavaCore/Module06/App.java
@@ -1,22 +1,15 @@
 package JavaCore.Module06;
 
-import JavaCore.Module05Poly.FlowerSaver;
 import JavaCore.Module05Poly.Garden.Chamomile;
-import JavaCore.Module05Poly.Garden.GardenFlower;
 import JavaCore.Module05Poly.Garden.Rose;
 import JavaCore.Module05Poly.Garden.Tulip;
 import JavaCore.Module05Poly.Interface.Flower;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
 
 public class App
 {
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException
+    public static void main(String[] args)
     {
         Flower rose = new Rose();
 
@@ -47,45 +40,6 @@ public class App
 
         // Так работает
         // FlowerSaver.save( bouquet1 );
-
-         MyHashMap<String, Flower> map = new MyHashMap<>();
-
-//         map.put( "Rose0", new Rose(  ) );
-//         map.put( "Rose1", new Rose(  ) );
-//         map.put( "Rose2", new Rose(  ) );
-//         map.put( "Rose3", new Rose(  ) );
-//         map.put( "Rose4", new Rose(  ) );
-//
-//         map.put( "Rose5", new Rose(  ) );
-//         map.put( "Rose6", new Rose(  ) );
-//         map.put( "Rose7", new Rose(  ) );
-//         map.put( "Rose8", new Rose(  ) );
-//         map.put( "Rose9", new Rose(  ) );
-//         map.put( "Rose10", new Rose(  ) );
-//         map.put( "Rose11", new Rose(  ) );
-//         map.put( "Rose12", new Rose(  ) );
-
-
-
-
-
-
     }
 
-//    private static void annotationProcess() throws IllegalAccessException, InstantiationException, InvocationTargetException
-//    {
-//        Class<MyArrayList> clazz = MyArrayList.class;
-//
-//        Method[] methods = clazz.getMethods();
-//
-//        for ( Method method: methods ){
-//            if ( method.isAnnotationPresent( Foo.class ) ){
-//
-//                Foo a = method.getAnnotation( Foo.class );
-//
-//                String[] f = a.tags();
-//
-//            }
-//        }
-//    }
 }
